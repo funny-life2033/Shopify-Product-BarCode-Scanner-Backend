c219506836f65de5487ebf7fac8ccd0e593fcf3e
MichaelCheers149 <michael.cheers@zzzzzzzz149.addy.io>
2024-10-30 09:48:58 +0900

    admin visible

--- controllers/user.js
+++ controllers/user.js
@@ -7,33 +7,27 @@ const getUsers = async (_, res) => {
     let users = await User.find({});
     let products = await Product.find({});
 
-    users = users
-
-      .map((user) => {
-        let productsCount = 0;
-        let i = 0;
-        while (true) {
-          if (i === products.length) break;
-          if (
-            products[i].uploadedBy &&
-            products[i].uploadedBy.equals(user._id)
-          ) {
-            productsCount++;
-            products.splice(i, 1);
-          } else {
-            i++;
-          }
+    users = users.map((user) => {
+      let productsCount = 0;
+      let i = 0;
+      while (true) {
+        if (i === products.length) break;
+        if (products[i].uploadedBy && products[i].uploadedBy.equals(user._id)) {
+          productsCount++;
+          products.splice(i, 1);
+        } else {
+          i++;
         }
+      }
 
-        return {
-          id: user._id,
-          username: user.username,
-          role: user.role,
-          email: user.email,
-          products: productsCount,
-        };
-      })
-      .filter((user) => user.role !== roles.admin);
+      return {
+        id: user._id,
+        username: user.username,
+        role: user.role,
+        email: user.email,
+        products: productsCount,
+      };
+    });
 
     res.json({ message: "Success!", users });
   } catch (error) {
@@ -52,7 +46,9 @@ const changeRoleOfUser = async (req, res) => {
       await user.save();
       res.json({ message: "Successfully changed!" });
     } else {
-      res.status(404).json({ message: "There is not such a user." });
+      if (!user) res.status(404).json({ message: "There is not such a user." });
+      else if (user.role === roles.admin)
+        res.status(404).json({ message: "You can't change admin's role" });
     }
   } catch (error) {
     console.log("error", error);
@@ -62,7 +58,15 @@ const changeRoleOfUser = async (req, res) => {
 
 const removeUser = async (req, res) => {
   try {
-    await User.findByIdAndDelete(req.params.userId);
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: "There is not such a user." });
+    }
+
+    if (user.role === roles.admin)
+      return res.status(404).json({ message: "You can't remove admin" });
+
+    await user.deleteOne();
     res.json({ message: "Successfully removed!" });
   } catch (error) {
     console.log("error", error);
